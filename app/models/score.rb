5db4728064476e1b0e1e469aae6869fae38810dc
RavWar <rav_war@mail.ru>
2013-12-24 16:34:03 +0400

    Check for opera before preloading sounds, add prelim top place

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,8 +1,8 @@
 class Score < ActiveRecord::Base
-  default_scope { order('value DESC') }
+  default_scope { order('value DESC, updated_at DESC') }
 
   def self.get_place score
-    record = where('value > ?', score).first
+    record = where('value > ?', score).last
     index  = all.index record
     index ? index + 2 : 1
   end
