fa740c786c27650a71f4c7f66f6be8f8bab19a94
RavWar <rav_war@mail.ru>
2014-12-24 00:29:16 +0300

    Change production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server '84.52.78.90', :app, :web, :db, primary: true
 set :port, 11111
-set :user, :velogame
+set :user, :funonbus
 set :user_home_dir, "/home/#{user}"
 set :keep_releases, 3
 set :unicorn_worker_processes, 3
